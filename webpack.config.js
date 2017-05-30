177ce7cf73fa6acefcf22a99f78f477859efb4e6
David Kocich <dkocich@gmail.com>
2017-05-30 23:20:41 +0200

    Change apostrophes to quotes in config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-if (process.NODE_ENV === 'development') {
-  module.exports = require('./webpack/webpack.config.dev.js');
+if (process.NODE_ENV === "development") {
+  module.exports = require("./webpack/webpack.config.dev.js");
 } else {
-  module.exports = require('./webpack/webpack.config.prod.js');
+  module.exports = require("./webpack/webpack.config.prod.js");
 }
