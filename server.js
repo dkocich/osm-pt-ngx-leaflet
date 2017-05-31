fab13b066116bae5811cc72710afab81531c6172
David Kocich <dkocich@gmail.com>
2017-05-31 16:46:16 +0200

    Fix quotes in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // in server.js
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Since the root/src dir contains our index.html
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + "/public/"));
 
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
