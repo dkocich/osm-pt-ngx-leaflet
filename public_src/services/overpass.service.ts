020aa857a4eeae1555d4e242b8c38866146da600
David Kocich <dkocich@gmail.com>
2017-06-25 23:17:37 +0200

    Add config + uploading functions

--- public_src/services/overpass.service.ts
+++ public_src/services/overpass.service.ts
@@ -4,6 +4,7 @@ import {MapService} from "./map.service";
 import {ProcessingService} from "./processing.service";
 import {StorageService} from "./storage.service";
 import {LoadingService} from "./loading.service";
+import {ConfigService} from "./config.service";
 
 const CONTINUOUS_QUERY: string = `
 [out:json][timeout:25][bbox:{{bbox}}];
@@ -65,5 +66,44 @@ export class OverpassService {
         headers.append("Content-Type", "application/X-www-form-urlencoded");
         let options: RequestOptions = new RequestOptions({headers: headers});
         return options;
+    }
+
+    /**
+     * TODO
+     * Put /api/0.6/changeset/create
+     */
+    private requestChangeSetId(): void {
+        let requestBody = "";
+        let options = this.setRequestOptions();
+        this.http.put(ConfigService.apiTestUrl + "/api/0.6/changeset/create", requestBody, options)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
+
+    /**
+     * TODO
+     * Update: PUT /api/0.6/changeset/#id
+     */
+    private updateChangeSet(): void {
+        let requestBody = "";
+        let options = this.setRequestOptions();
+        this.http.put(ConfigService.apiTestUrl + "/api/0.6/changeset/create", requestBody, options)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
+
+    /**
+     * TODO
+     * Close: PUT /api/0.6/changeset/#id/close
+     */
+    private closeChangeSet(id: number): void {
+        let requestBody = "";
+        let options = this.setRequestOptions();
+        this.http.put(ConfigService.apiTestUrl + "/api/0.6/changeset/" + id + "close", requestBody, options)
+            .subscribe(response => {
+                console.log(response);
+            });
     }
 }
