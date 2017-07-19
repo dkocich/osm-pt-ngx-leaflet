2d2295d85667c1ba34769f539d79693fc0858838
David Kocich <dkocich@gmail.com>
2017-07-19 09:42:33 +0200

    Filter object keys with hasOwnProperty

--- public_src/components/pipes/keys.pipe.ts
+++ public_src/components/pipes/keys.pipe.ts
@@ -5,7 +5,9 @@ export class KeysPipe implements PipeTransform {
     public transform(value, args: string[]): any {
         const keys = [];
         for (const key in value) {
-            keys.push({ key, value: value[key] });
+            if (value.hasOwnProperty(key)) {
+                keys.push({ key, value: value[key] });
+            }
         }
         return keys;
     }
