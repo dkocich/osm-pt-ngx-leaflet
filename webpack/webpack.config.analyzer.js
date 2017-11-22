0c91f098d662297c455f86d18b91d010cb47a686
David Kocich <dkocich@gmail.com>
2017-11-22 23:24:38 +0100

    Add WP bundle analysis tools (analyzer-treemap, visualizer-sunburst)
    - analyzer - run npm task to get browser opened on the page with a chart
    - visualizer - open manually statistics.html in the /public folder

---
+++ webpack/webpack.config.analyzer.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const srcDir = "public_src";
+const outputDir = "../public";
+
+module.exports = {
+    devtool: "source-map",
+    entry: {
+        app: path.resolve(srcDir, "bootstrap.ts")
+    },
+    output: {
+        path: path.resolve(__dirname, outputDir),
+        filename: "[name].[hash].bundle.js",
+        sourceMapFilename: "[name].[hash].map",
+        chunkFilename: "[id].[hash].chunk.js"
+    },
+    resolve: {
+        extensions: [".ts", ".component.ts", ".service.ts", ".js", ".component.html", ".component.less", ".less", ".css"]
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, enforce: "pre", loader: "tslint-loader" },
+            { test: /(\.component|\.service|)\.ts$/, use: ["ts-loader"] },
+            { test: /\.component\.html$/, use: [{ loader: "html-loader", options: { minimize: false } }] },
+            { test: /(\.component|)\.less$/, use: ["to-string-loader", "css-loader", "less-loader"] },
+            { test: /\.css$/, use: ExtractTextPlugin.extract({ fallback: "style-loader", use: "css-loader" })},
+            { test: /\.(png|gif|jpg|ico|svg)$/, use:[{ loader: "file-loader", options: { name: "images/[name].[ext]"} } ]},
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use:[{ loader: "file-loader", options: { name: "fonts/[name].[ext]"} } ]},
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use:[{ loader: "file-loader", options: { name: "fonts/[name].[ext]"} } ]},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use:[{ loader: "file-loader", options: { name: "fonts/[name].[ext]"} } ]},
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use:[{ loader: "file-loader", options: { name: "fonts/[name].[ext]"} } ]},
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use:[{ loader: "file-loader", options: { name: "fonts/[name].[ext]"} } ]}
+        ],
+        noParse: [ path.join(__dirname, "node_modules", "angular2", "bundles") ]
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].[contenthash].css"),
+        new BundleAnalyzerPlugin()
+    ]
+};
