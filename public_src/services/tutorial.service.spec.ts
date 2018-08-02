be20d79730f5e7f3ed3c1e48f61787956238a424
anushasingh <anushasingh996@gmail.com>
2018-08-03 00:13:07 +0530

    add spec files

---
+++ public_src/services/tutorial.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TutorialService } from './tutorial.service';
+
+describe('TutorialService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TutorialService]
+    });
+  });
+
+  it('should be created', inject([TutorialService], (service: TutorialService) => {
+    expect(service).toBeTruthy();
+  }));
+});
