e01ede726755232d2d27dfc286460b18f692377b
dkocich <dkocich@gmail.com>
2022-12-08 23:23:51 +0100

    chore: del unused/old tests, snyk file and deps

--- src/pipes/tags-prop-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TagsPropPipe } from './tags-prop.pipe';
-
-describe('TagsPropPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TagsPropPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- test/main.js
+++
@@ -1,7 +0,0 @@
-describe("Meaningful Test", () => {
-  it("1 + 1 => 2", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
-
-import "./main.spec";
